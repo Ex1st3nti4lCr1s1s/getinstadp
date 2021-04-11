31a8bed127aaa9323373df44a4a8abc38b5da7d2
Jay <duttjay255@gmail.com>
2021-04-11 21:23:15 +0530

    Added program

---
+++ getinstadp.py
@@ -0,0 +1,26 @@
+import requests
+import os
+import json
+import shutil
+import json
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+}
+
+
+fp = open("names.txt", "r")
+
+for cnt, line in enumerate(fp):
+    new = str(line.strip())
+    url = "https://www.instagram.com/"+new+"/?__a=1"
+    print(url)
+    link = str(url)
+
+    response = requests.get(link, headers=header).json()
+
+    hd_image_location = response["graphql"]["user"]["profile_pic_url_hd"]
+
+    hd_image_response = requests.get(hd_image_location, stream=True)
+    with open(f"{new}.jpg", "wb") as out_file:
+        shutil.copyfileobj(hd_image_response.raw, out_file)
